e14ef1bc4d262c45651d8d7e6639aec6b2342251
HandOfGod94 <gahan94rakh@gmail.com>
2016-03-04 02:30:41 +0530

    Feature added: practice and problem dynamic page

--- judge_temp/views.py
+++ judge_temp/views.py
@@ -1,7 +1,7 @@
 from judge_temp import app
 from flask import render_template, request
 from models import User, Problem
-from models import FileType
+from models import TestcaseFileType
 
 import controllers as ctrl
 
@@ -14,7 +14,7 @@ def index():
 def register():
     if request.method=='POST':
         # TODO: enclose this with try catch
-        user = User(registrationNumber=request.form['registrationNumber'],
+        user = User(id=request.form['registrationNumber'],
                     firstName=request.form['firstName'],
                     lastName=request.form['lastName'],
                     password=request.form['password'],
@@ -22,7 +22,10 @@ def register():
                     contactNo=request.form['contactNo'],
                     branch=request.form['branch'])
         profilePic = request.files['profilePic']
-        ctrl.saveProfilePic(profilePic, user.registrationNumber)
+        ctrl.saveProfilePic(profilePic, user.id)
+        if profilePic!=None:
+            profilePicExtension = ctrl.getExtensionOfFile(profilePic.filename)
+        user.profilePicExtension = profilePicExtension
         ctrl.insertToDb(user)
     return render_template('registration_page.html')
 
@@ -37,8 +40,27 @@ def addProblem():
                           sampleInput=request.form['sampleInput'],
                           sampleOutput=request.form['sampleOutput'],
                           difficultyLevel=request.form['difficultyLevel'],
-                          category=request.form['category'])
+                          category=request.form['category'],
+                          successfulSubmission=0,
+                          attempts=0,
+                          inputFormat=request.form['inputFormat'],
+                          outputFormat=request.form['outputFormat'],
+                          explanation=request.form['explanation'])
         ctrl.insertToDb(problem)
-        ctrl.saveTestCases(request.files.getlist('inputFiles'),problem.id,FileType.INPUT)
-        ctrl.saveTestCases(request.files.getlist('outputFiles'),problem.id,FileType.OUTPUT)
+        ctrl.saveTestCases(request.files.getlist('inputFiles'),problem.id,TestcaseFileType.INPUT)
+        ctrl.saveTestCases(request.files.getlist('outputFiles'),problem.id,TestcaseFileType.OUTPUT)
     return render_template('add_problem.html')
+
+
+@app.route('/<userId>/practice')
+def practice(userId):
+    user = ctrl.selectFromDb(User,key=userId)
+    problems = ctrl.selectFromDb(Problem)
+    return render_template('practice.html',user=user,problems=problems)
+
+
+@app.route('/<userId>/practice/<int:problemId>')
+def problemSolving(userId,problemId):
+    user = ctrl.selectFromDb(User,key=userId)
+    problem = ctrl.selectFromDb(Problem,key=problemId)
+    return render_template('problem.html',user=user,problem=problem)
